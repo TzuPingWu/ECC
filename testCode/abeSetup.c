700bfbeaf857947e2725727157d457ec44f8ab9f
TzuPingWu <bottle7839@hotmail.com>
2015-04-09 17:25:10 +0800

    Edit the abeSetup.c, abeSetup.h, abe.c and new the publicKey.key and msk.key

--- testCode/abeSetup.c
+++ testCode/abeSetup.c
@@ -21,40 +21,72 @@ void setupPairing(pairing_t *pairing){
 //	pbc_param_out_str(stdout, param);
 	return;
 }
-/*
-void setup(int attrNo,pairing_t pairing){
-	
-	int count = 0;
-	setupPairing(&pairing);//setup pairing first
-	if(pairing_is_symmetric(pairing)){
-		element_t g;//the generator of G
-		element_init_G1(g,pairing);//initial the generator g
-		// initial the random group elements h_1...h_attrNo 
-		//which belog to G and are associated with the attrNo 
-		//attributes in the system.
-		//
-		element_t h[attrNo];
+
+void setup(int attrNo,pairing_t *pairing){
+	int count = 0;//the index of the attribute array
+	setupPairing(pairing);//setup pairing first
+        if(pairing_is_symmetric(*pairing)){
+                element_t g;//the generator of G
+                element_init_G2(g,*pairing);//initial the generator g
+                element_random(g);
+                /* initial the random group elements h_1...h_attrNo 
+                which belog to G and are associated with the attrNo 
+                attributes in the system.
+                */
+      
+                element_t h[attrNo];
+                while(count!=attrNo){
+                        element_init_G2(h[count],*pairing);
+                        element_random(h[count]);
+                        count++;
+                }//end of while, finish generating the h_1...h_attrNo
+                element_t alpha;
+                element_t a;
+                //initial the alpha and a in Z_p        
+                element_init_Zr(alpha,*pairing);
+                element_init_Zr(a,*pairing);
+                element_random(alpha);
+                element_random(a);
+                //public key e(g,g)^alpha
+                element_t pubKey;
+                element_t gAlpha;
+		element_t gA;
+                element_init_GT(pubKey,*pairing);//initial the publicKey
+                element_init_G2(gAlpha,*pairing);//initial the gAlpha
+		element_init_G2(gA,*pairing);//initial the gA
+                element_pow_zn(gAlpha,g,alpha);//gAlpha=g^alpha
+		element_pow_zn(gA,g,a);//gA=g^a
+                element_pairing(pubKey,g,gAlpha);//publicKey = e(g,g^alpha) = e(g,g)^alpha
+                //Master secret key
+                element_t msk;
+                element_init_G2(msk,*pairing);
+                element_set(msk,gAlpha);//msk = g^alpha
+                //write the master key and public key to file
+		FILE* fPubKey = fopen("publicKey.key","w");//file pointer to the public key
+		FILE* fMsk = fopen("msk.key","w");//file pointer to the master key
+		element_fprintf(fPubKey,"g=%B\n",g);
+		element_fprintf(fPubKey,"e(g,gAlpha)=%B\n",pubKey);
+		element_fprintf(fPubKey,"gA=%B\n",gA);
+		count = 0;
 		while(count!=attrNo){
-			element_init_G1(h[count],pairing);
+			element_fprintf(fPubKey,"h[%d]=%B\n",count,h[count]);
+			element_clear(h[count]);
 			count++;
-		}//end of while, finish generating the h_1...h_attrNo
-		element_t alpha;
-		element_t a;
-		//initial the alpha and a in Z_p	
-		element_init_Zr(alpha,pairing);
-		element_init_Zr(a,pairing);
-		//public key e(g,g)^alpha
-		element_t pubKey;
-		element_t gAlpha;
-		element_init_GT(pubKey,pairing);
-		element_init_G1(gAlpha);
-		element_t gAlpha =  element_mul(gAlpha,g,alpha);//g^alpha
-		element_pairing(pubKey,g,g^alpha);//e(g,g^alpha) = e(g,g)^alpha
-		//Master secret key
-		element_t msk = gAlpha;
-		
-	}else{
-		PBC_ASSERT(pairing_is_symmetric(pairing),"The pairing isn't symmetric!");
-	}
+		}
+		element_fprintf(fMsk,"MSK=%B\n",msk);
+		//close the file pointer and clear all the element
+		fclose(fPubKey);
+		fclose(fMsk);
+		element_clear(g);
+		element_clear(a);
+		element_clear(alpha);
+		element_clear(gAlpha);
+		element_clear(gA);
+		element_clear(pubKey);
+		element_clear(msk);
+        }else{
+                PBC_ASSERT(pairing_is_symmetric(*pairing),"The pairing isn't symmetric!\n");
+        }
+
 }//end of setup
-*/
+
--- testCode/abeSetup.h
+++ testCode/abeSetup.h
@@ -6,4 +6,6 @@ void generatePrime(mpz_t *p,int sBit);
 //generate prime number
 void setupPairing(pairing_t *pairing);
 //setup the pairing
+void setup(int attrNo,pairing_t *pairing);
+//the first setup of cp-abe protocol -- setup
 #endif
